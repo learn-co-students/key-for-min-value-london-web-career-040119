dfce319da8c58ca2652122ad4ebdfd6765787efc
Francisco Costa <francisco@fcosta.pt>
2019-03-18 14:21:49 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,5 +5,14 @@
 
 
 def key_for_min_value(name_hash)
+  min_name = nil
+  min_value = nil
 
+  name_hash.each do |name, value|
+    if min_value == nil || value < min_value
+      min_name = name
+      min_value = value
+    end
+  end
+  min_name
 end
