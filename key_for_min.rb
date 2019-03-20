e335902c3c1fd44429235ff52e6dbb4fce62f9ec
rebeccaswebsite <rebecca.huseyin.14@ucl.ac.uk>
2019-03-20 15:40:59 +0000

    added method to find smallest value in a hash

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,16 @@
+require "pry"
+
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+	smallest_value = nil
+	associated_key = nil 
+	name_hash.collect do |key, value|
+		if smallest_value == nil || value < smallest_value
+			smallest_value = value 
+			associated_key = key
+	  	end
+	end
+	associated_key
 end
