958243f7923e2b28acf44858dfb70cbb3254ad8a
Xabier C <xabier_abia@hotmail.com>
2019-03-20 11:27:37 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,14 +3,15 @@
 
 def key_for_min_value(name_hash)
   smallest_value = 0 
-  smallest_key = nil 
-
+  smallest_key = nil
+  
   name_hash.collect do |key, value|
     if smallest_value == 0 || value < smallest_value
       smallest_value = value
       smallest_key = key
     end
   end
+  
   smallest_key
 end
 
