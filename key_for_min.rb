fd86cd18d9d35868646e90703d70eaf414bea077
Kevin Kamanda <kevkamanda@gmail.com>
2019-03-28 13:24:53 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,20 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-def key_for_min_value(name_hash)
+def key_for_min_value(birthday_hash)
+  min_kid = nil
+  min_age = nil
 
+#using each will allow you to return the single value
+
+  birthday_hash.each do |kids_name, age|
+    if min_age == nil || age < min_age 
+      
+#how to sort age as a min_value
+      
+        min_kid = kids_name
+        min_age = age
+    end
+  end
+  min_kid
 end
